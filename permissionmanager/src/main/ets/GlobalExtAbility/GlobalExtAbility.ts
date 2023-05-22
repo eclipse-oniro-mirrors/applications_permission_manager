31e4335719a30d1643625fc0f9a8f6ba4d37547b
fanchenxuan <fanchenxuan@huawei.com>
2023-05-22 17:37:11 +0800

    通用规范告警
    Signed-off-by: fanchenxuan <fanchenxuan@huawei.com>

--- permissionmanager/src/main/ets/GlobalExtAbility/GlobalExtAbility.ts
+++ permissionmanager/src/main/ets/GlobalExtAbility/GlobalExtAbility.ts
@@ -18,64 +18,64 @@ import window from '@ohos.window';
 import display from '@ohos.display';
 import deviceInfo from '@ohos.deviceInfo';
 
-var TAG = "PermissionManager_Log:";
-const BG_COLOR = '#00000000'
-let bottomPopoverTypes = ['default', 'phone']
+const TAG = 'PermissionManager_Log:';
+const BG_COLOR = '#00000000';
+let bottomPopoverTypes = ['default', 'phone'];
 
 export default class GlobalExtensionAbility extends extension {
-    /**
-     * Lifecycle function, called back when a service extension is started for initialization.
-     */
-    onCreate(want) {
-        console.info(TAG + "ServiceExtensionAbility onCreate, ability name is " + want.abilityName);
+  /**
+  * Lifecycle function, called back when a service extension is started for initialization.
+  */
+  onCreate(want): void {
+    console.info(TAG + 'ServiceExtensionAbility onCreate, ability name is ' + want.abilityName);
 
-        globalThis.globalContext = this.context;
-        globalThis.globalState = want.parameters['ohos.sensitive.resource']
-        console.info(TAG + "want: " + JSON.stringify(want))
+    globalThis.globalContext = this.context;
+    globalThis.globalState = want.parameters['ohos.sensitive.resource'];
+    console.info(TAG + 'want: ' + JSON.stringify(want));
 
-        try {
-            let dis = display.getDefaultDisplaySync();
-            let navigationBarRect = {
-                left: 0,
-                top: 0,
-                width: dis.width,
-                height: dis.height
-            }
-            let isVertical = dis.width > dis.height ? false : true
-            globalThis.isBottomPopover = bottomPopoverTypes.includes(deviceInfo.deviceType) && isVertical
-            this.createWindow("globalDialog", window.WindowType.TYPE_KEYGUARD, navigationBarRect)
-        } catch (exception) {
-            console.error(TAG + 'Failed to obtain the default display object. Code: ' + JSON.stringify(exception));
-        };
+    try {
+      let dis = display.getDefaultDisplaySync();
+      let navigationBarRect = {
+        left: 0,
+        top: 0,
+        width: dis.width,
+        height: dis.height
+      };
+      let isVertical = dis.width > dis.height ? false : true;
+      globalThis.isBottomPopover = bottomPopoverTypes.includes(deviceInfo.deviceType) && isVertical;
+      this.createWindow('globalDialog', window.WindowType.TYPE_KEYGUARD, navigationBarRect);
+    } catch (exception) {
+      console.error(TAG + 'Failed to obtain the default display object. Code: ' + JSON.stringify(exception));
+    };
+  }
+
+  /**
+  * Lifecycle function, called back when a service extension is started or recall.
+  */
+  onRequest(want, startId): void {
+    console.info(TAG + 'ServiceExtensionAbility onRequest. start id is ' + startId);
+  }
+
+  /**
+  * Lifecycle function, called back before a service extension is destroyed.
+  */
+  onDestroy(): void {
+    console.info(TAG + 'ServiceExtensionAbility onDestroy.');
+    globalThis.globalWin.destroy();
+  }
+
+  private async createWindow(name: string, windowType: number, rect): Promise<void> {
+    console.info(TAG + 'create window');
+    try {
+      const win = await window.createWindow({ ctx: globalThis.globalContext, name, windowType });
+      globalThis.globalWin = win;
+      await win.moveWindowTo(rect.left, rect.top);
+      await win.resize(rect.width, rect.height);
+      await win.setUIContent('pages/globalSwitch');
+      await win.setWindowBackgroundColor(BG_COLOR);
+      await win.showWindow();
+    } catch {
+      console.info(TAG + 'window create failed!');
     }
-
-    /**
-     * Lifecycle function, called back when a service extension is started or recall.
-     */
-    onRequest(want, startId) {
-        console.info(TAG + "ServiceExtensionAbility onRequest. start id is " + startId);
-    }
-
-    /**
-     * Lifecycle function, called back before a service extension is destroyed.
-     */
-    onDestroy() {
-        console.info(TAG + "ServiceExtensionAbility onDestroy.");
-        globalThis.globalWin.destroy()
-    }
-
-    private async createWindow(name: string, windowType: number, rect) {
-        console.info(TAG + "create window")
-        try {
-            const win = await window.createWindow({ ctx: globalThis.globalContext, name, windowType })
-            globalThis.globalWin = win
-            await win.moveWindowTo(rect.left, rect.top)
-            await win.resize(rect.width, rect.height)
-            await win.setUIContent('pages/globalSwitch')
-            await win.setWindowBackgroundColor(BG_COLOR)
-            await win.showWindow()
-        } catch {
-            console.info(TAG + "window create failed!")
-        }
-    }
+  }
 };
--- permissionmanager/src/main/ets/MainAbility/MainAbility.ts
+++ permissionmanager/src/main/ets/MainAbility/MainAbility.ts
@@ -13,77 +13,77 @@
  * limitations under the License.
  */
 
-import UIAbility from '@ohos.app.ability.UIAbility'
-import bundle from "@ohos.bundle";
+import UIAbility from '@ohos.app.ability.UIAbility';
+import bundle from '@ohos.bundle';
 
-var TAG = "PermissionManager_MainAbility:";
+const TAG = 'PermissionManager_MainAbility:';
 const PARAMETER_BUNDLE_FLAG = 16;
 const USERID = 100;
 const noNeedDisplayApp: string[] = [
-    "com.ohos.launcher"
-]
+  'com.ohos.launcher'
+];
 
 export default class MainAbility extends UIAbility {
-    onCreate(want, launchParam) {
-        console.log(TAG + "MainAbility onCreate, ability name is " + want.abilityName + ".");
-        globalThis.context = this.context;
-        globalThis.allBundleInfo = [];
-        globalThis.allUserPermissions = []
-        globalThis.allGroups = []
-        globalThis.permissionLabels = {}
-        globalThis.initialGroups = []
-    }
+  onCreate(want, launchParam): void {
+    console.log(TAG + 'MainAbility onCreate, ability name is ' + want.abilityName + '.');
+    globalThis.context = this.context;
+    globalThis.allBundleInfo = [];
+    globalThis.allUserPermissions = [];
+    globalThis.allGroups = [];
+    globalThis.permissionLabels = {};
+    globalThis.initialGroups = [];
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log(TAG + "MainAbility onWindowStageCreate.");
+  onWindowStageCreate(windowStage): void {
+    // Main window is created, set main page for this ability
+    console.log(TAG + 'MainAbility onWindowStageCreate.');
 
-        bundle.getAllBundleInfo(PARAMETER_BUNDLE_FLAG).then(async(bundleInfos) => {
-            if (bundleInfos.length <= 0) {
-                console.info(TAG + 'bundle.getAllBundleInfo result.length less than or equal to zero');
-                return;
-            }
-            for (let i = 0; i < bundleInfos.length; i++) {
-                var info = bundleInfos[i];
-                // Filter blank icon icon and text label resources
-                try {
-                    await bundle.queryAbilityByWant({
-                        bundleName: info.name,
-                        action: "action.system.home",
-                        entities: ["entity.system.home"]
-                    }, bundle.BundleFlag.GET_ABILITY_INFO_WITH_APPLICATION, USERID);
-                } catch(e) {
-                    console.log(TAG + 'queryAbilityByWant catch error: ' + JSON.stringify(e))
-                    continue;
-                }
+    bundle.getAllBundleInfo(PARAMETER_BUNDLE_FLAG).then(async(bundleInfos) => {
+      if (bundleInfos.length <= 0) {
+        console.info(TAG + 'bundle.getAllBundleInfo result.length less than or equal to zero');
+        return;
+      }
+      for (let i = 0; i < bundleInfos.length; i++) {
+        let info = bundleInfos[i];
+        // Filter blank icon icon and text label resources
+        try {
+          await bundle.queryAbilityByWant({
+            bundleName: info.name,
+            action: 'action.system.home',
+            entities: ['entity.system.home']
+          }, bundle.BundleFlag.GET_ABILITY_INFO_WITH_APPLICATION, USERID);
+        } catch (error) {
+          console.log(TAG + 'queryAbilityByWant catch error: ' + JSON.stringify(error));
+          continue;
+        }
 
-                if (noNeedDisplayApp.indexOf(info.name) != -1) {
-                    continue;
-                }
-                globalThis.initialGroups.push(info)
-            }
-            windowStage.setUIContent(this.context, "pages/authority-management", null);
-        }).catch((error) => {
-            console.error(TAG + 'bundle.getAllBundleInfo failed. Cause: ' + JSON.stringify(error));
-        })
-    }
+        if (noNeedDisplayApp.indexOf(info.name) !== -1) {
+          continue;
+        }
+        globalThis.initialGroups.push(info);
+      }
+      windowStage.setUIContent(this.context, 'pages/authority-management', null);
+    }).catch((error) => {
+      console.error(TAG + 'bundle.getAllBundleInfo failed. Cause: ' + JSON.stringify(error));
+    })
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log(TAG + "MainAbility onForeground.");
-    }
+  onForeground(): void {
+    // Ability has brought to foreground
+    console.log(TAG + 'MainAbility onForeground.');
+  }
 
-    onBackground() {
-        // Ability has back to background
-        console.log(TAG + "MainAbility onBackground.");
-    }
+  onBackground(): void {
+    // Ability has back to background
+    console.log(TAG + 'MainAbility onBackground.');
+  }
 
-    onDestroy() {
-        console.log(TAG + "MainAbility onDestroy.");
-    }
+  onDestroy(): void {
+    console.log(TAG + 'MainAbility onDestroy.');
+  }
 
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        console.log(TAG + "MainAbility onWindowStageDestroy.");
-    }
+  onWindowStageDestroy(): void {
+    // Main window is destroyed, release UI related resources
+    console.log(TAG + 'MainAbility onWindowStageDestroy.');
+  }
 };
