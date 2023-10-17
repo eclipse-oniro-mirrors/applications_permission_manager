ca5bc0d7a53537a812034965bbb9a6c4f10c0cd5
fanchenxuan <fanchenxuan@huawei.com>
2023-10-17 14:28:14 +0800

    多ability整合
    Signed-off-by: fanchenxuan <fanchenxuan@huawei.com>

--- permissionmanager/src/main/ets/MainAbility/MainAbility.ts
+++ permissionmanager/src/main/ets/MainAbility/MainAbility.ts
@@ -25,13 +25,77 @@ export default class MainAbility extends UIAbility {
   onCreate(want, launchParam): void {
     console.log(TAG + 'MainAbility onCreate, ability name is ' + want.abilityName + '.');
 
+    globalThis.bundleName = want.parameters.bundleName;
+    GlobalContext.store('bundleName', want.parameters.bundleName);
     GlobalContext.store('context', this.context);
   }
 
   onWindowStageCreate(windowStage): void {
     // Main window is created, set main page for this ability
     console.log(TAG + 'MainAbility onWindowStageCreate.');
+    globalThis.windowStage = windowStage;
 
+    if (globalThis.bundleName) {
+      globalThis.currentApp = globalThis.bundleName;
+      this.getSperifiedApplication(globalThis.bundleName);
+    } else {
+      globalThis.currentApp = 'all';
+      this.getAllApplications();
+    }
+  }
+
+  onNewWant(want): void {
+    console.log(TAG + 'MainAbility onNewWant. want: ' + JSON.stringify(want));
+    console.log(TAG + 'MainAbility onNewWant. bundleName: ' + JSON.stringify(want.parameters.bundleName));
+
+    let bundleName = want.parameters.bundleName ? want.parameters.bundleName : 'all';
+    if (globalThis.currentApp === 'all') {
+      if (globalThis.currentApp !== bundleName) {
+        console.log(TAG + 'MainAbility onNewWant. all -> app');
+        globalThis.windowStage.setUIContent(this.context, 'pages/transition', null);
+        globalThis.currentApp = bundleName;
+        GlobalContext.store('bundleName', bundleName);
+        this.getSperifiedApplication(bundleName);
+      }
+    } else {
+      if (bundleName === 'all') {
+        console.log(TAG + 'MainAbility onNewWant. app -> all');
+        globalThis.windowStage.setUIContent(this.context, 'pages/transition', null);
+        globalThis.currentApp = 'all';
+        this.getAllApplications();
+      } else {
+        if (globalThis.currentApp !== bundleName) {
+          console.log(TAG + 'MainAbility onNewWant. app -> app');
+          globalThis.windowStage.setUIContent(this.context, 'pages/transition', null);
+          globalThis.currentApp = bundleName;
+          GlobalContext.store('bundleName', bundleName);
+          this.getSperifiedApplication(bundleName);
+        }
+      }
+    }
+
+  }
+
+  onForeground(): void {
+    // Ability has brought to foreground
+    console.log(TAG + 'MainAbility onForeground.');
+  }
+
+  onBackground(): void {
+    // Ability has back to background
+    console.log(TAG + 'MainAbility onBackground.');
+  }
+
+  onDestroy(): void {
+    console.log(TAG + 'MainAbility onDestroy.');
+  }
+
+  onWindowStageDestroy(): void {
+    // Main window is destroyed, release UI related resources
+    console.log(TAG + 'MainAbility onWindowStageDestroy.');
+  }
+
+  getAllApplications(): void {
     const flag = bundleManager.BundleFlag.GET_BUNDLE_INFO_WITH_APPLICATION | bundleManager.BundleFlag.GET_BUNDLE_INFO_WITH_REQUESTED_PERMISSION;
     let accountManager = account_osAccount.getAccountManager();
     try {
@@ -62,32 +126,43 @@ export default class MainAbility extends UIAbility {
             initialGroups.push(info);
           }
           let storage: LocalStorage = new LocalStorage({ 'initialGroups': initialGroups });
-          windowStage.loadContent('pages/authority-management', storage);
+          globalThis.windowStage.loadContent('pages/authority-management', storage);
         }).catch((error) => {
           console.error(TAG + 'bundle.getAllBundleInfo failed. Cause: ' + JSON.stringify(error));
-        })
+        });
       });
     } catch (e) {
       console.error(TAG + 'getActivatedOsAccountLocalIds exception: ' + JSON.stringify(e));
     }
   }
 
-  onForeground(): void {
-    // Ability has brought to foreground
-    console.log(TAG + 'MainAbility onForeground.');
-  }
+  getSperifiedApplication(bundleName): void {
+    const flag = bundleManager.BundleFlag.GET_BUNDLE_INFO_WITH_APPLICATION | bundleManager.BundleFlag.GET_BUNDLE_INFO_WITH_REQUESTED_PERMISSION;
+    bundleManager.getBundleInfo(bundleName, flag).then(bundleInfo => {
+      let reqPermissions: Array<string> = [];
+      bundleInfo.reqPermissionDetails.forEach(item => {
+        reqPermissions.push(item.name);
+      });
 
-  onBackground(): void {
-    // Ability has back to background
-    console.log(TAG + 'MainAbility onBackground.');
-  }
-
-  onDestroy(): void {
-    console.log(TAG + 'MainAbility onDestroy.');
-  }
-
-  onWindowStageDestroy(): void {
-    // Main window is destroyed, release UI related resources
-    console.log(TAG + 'MainAbility onWindowStageDestroy.');
+      let info = {
+        'bundleName': bundleInfo.name,
+        'api': bundleInfo.targetVersion,
+        'tokenId': bundleInfo.appInfo.accessTokenId,
+        'icon': '',
+        'iconId': bundleInfo.appInfo.iconId,
+        'label': '',
+        'labelId': bundleInfo.appInfo.labelId,
+        'permissions': reqPermissions,
+        'groupId': [],
+        'zhTag': '',
+        'indexTag': '',
+        'language': ''
+      };
+      GlobalContext.store('applicationInfo', info);
+      globalThis.windowStage.setUIContent(this.context, 'pages/application-secondary', null);
+    }).catch(() => {
+      console.log(TAG + 'MainAbility getSperifiedApplication failed.');
+      this.context.terminateSelf();
+    });
   }
 };
